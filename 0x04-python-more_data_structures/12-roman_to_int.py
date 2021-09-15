b4105a1c80084d2de61b1895a68dc765913cfa3f
khouzi <62845331+khouzi@users.noreply.github.com>
2021-09-15 20:32:46 +0100

    Fix file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,5 @@
 def roman_to_int(roman_string):
-    if roman_string is str:
-        return 0
-    elif not roman_string:
+    if not roman_string:
         return 0
     else:
         R = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
