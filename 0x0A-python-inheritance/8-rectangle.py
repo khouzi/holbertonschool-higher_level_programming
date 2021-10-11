1dca7479411a02e90d66385d4b4562c09be0ed58
khouzi <62845331+khouzi@users.noreply.github.com>
2021-10-11 11:10:22 -0700

    pep8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,34 +2,7 @@
 """ BaseGeometry class """
 
 
-class BaseGeometry:
-    """
-    class BaseGeometry
-    """
-    def area(self):
-        """
-        area geometry function
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        integer validator value
-        Args:
-            name(string): name
-            value(int): given value
-        Raises:
-            TypeError: when value is not integer
-            ValueError: when value is less or equal to 0
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-"""
-Rectangle Module
-"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -37,13 +10,12 @@ class Rectangle(BaseGeometry):
     Rectangle class
     """
     def __init__(self, width, height):
-        """
-        Init function
+        """validate and initialize width and height
         Args:
-            width(int): the width of rectangle
-            height(int): the height of the rectangle
+            width (int): private
+            height (int): private
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
