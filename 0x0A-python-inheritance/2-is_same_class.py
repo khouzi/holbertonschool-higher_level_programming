cfe54fb7b70c8ffe443c65af6a13eb1b7cdab750
khouzi <62845331+khouzi@users.noreply.github.com>
2021-10-11 11:00:27 -0700

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-""" is_same_class Module """
+"""compare obj to instance"""
 
 
 def is_same_class(obj, a_class):
-    """
-    function to check if an object is an instance of a class
-    Args:
-            obj (object): the object
-            a_class (class): the class
-    Returns:
-            True: if object is an instance of the class
-            False: if object is not an instance of the class
-    """
-    return isinstance(obj, a_class)
+    """function to comapre obj to instance"""
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
