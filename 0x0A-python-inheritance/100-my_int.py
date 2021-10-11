9bf3d7f5c0a84593a854a9c2cd5b6995d42880ac
khouzi <khouzem.boussaid@gmail.com>
2021-10-11 11:42:24 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,24 +5,26 @@ rebel class
 
 
 class MyInt(int):
-	"""
-	MyInt class
-	"""
-	def __eq__(self, other):
-		"""
-		Eq function
-		Args:
-			other(int): number to operate on
-		Returns:
-            the inverted operator !=
-		"""
-		return super().__ne__(other)
-	def __ne__(self, other):
-		"""
-		Ne function
-		Args:
-			other(int): number to operate on
-		Returns:
-            the inverted operator ==
-		"""
-		return super().__eq__(other)
+    """
+    MyInt class
+    """
+
+    def __eq__(self, other):
+        """
+        Eq function
+        Args:
+                other(int): number to operate on
+        Returns:
+    the inverted operator !=
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Ne function
+        Args:
+                other(int): number to operate on
+        Returns:
+    the inverted operator ==
+        """
+        return super().__eq__(other)
