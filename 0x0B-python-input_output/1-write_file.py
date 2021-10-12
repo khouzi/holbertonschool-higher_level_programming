675fadae85a1e0770a67edacd95d7d874f4d7e53
khouzi <khouzem.boussaid@gmail.com>
2021-10-12 09:40:36 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,13 +5,13 @@ function write file
 
 
 def write_file(filename="", text=""):
-	"""
-    write file function
-    Args:
-        filename(string): the given file name
-        text(string): the text
-    Returns:
-        number of characters written.
     """
-	with open(filename, mode ="w", encoding="utf-8") as myfile:
-		return myfile.write(text)
+write file function
+Args:
+    filename(string): the given file name
+    text(string): the text
+Returns:
+    number of characters written.
+"""
+    with open(filename, mode="w", encoding="utf-8") as myfile:
+        return myfile.write(text)
