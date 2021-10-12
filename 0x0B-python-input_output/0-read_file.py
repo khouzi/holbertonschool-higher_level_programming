44acd40c02c83193b9a0ff923ec84cbf4a88d3f5
khouzi <khouzem.boussaid@gmail.com>
2021-10-12 09:40:22 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,10 +5,10 @@ function read file
 
 
 def read_file(filename=""):
-	"""
-    Read file function
-    Args:
-        filename(string): the given file name
     """
-	with open(filename, encoding="utf-8") as myfile:
-		print(myfile.read())
+Read file function
+Args:
+    filename(string): the given file name
+"""
+    with open(filename, encoding="utf-8") as myfile:
+        print(myfile.read())
