cc8c88087548cfb44a8b1022c0c85ad5442445d9
khouzi <khouzem.boussaid@gmail.com>
2021-10-12 19:54:15 +0200

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         retrieves a dictionary representation
         of a Student instance
