0fc7719566e870824d15491451e1fe2e82afbeed
khouzi <khouzem.boussaid@gmail.com>
2021-10-12 19:57:35 +0200

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,7 +2,7 @@
 """ student module """
 
 
-class Student:
+class Student():
     """student class"""
 
     def __init__(self, first_name, last_name, age):
@@ -19,11 +19,11 @@ class Student:
         of a Student instance
         """
 
-    if attrs is None:
+        if attrs is None:
             return self.__dict__
         else:
             dic = {}
-            for att in attrs:
-                if att in self.__dict__.keys():
-                    dic[att] = self.__dict__[att]
-            return dic
+        for att in attrs:
+            if att in self.__dict__.keys():
+                dic[att] = self.__dict__[att]
+        return dic
