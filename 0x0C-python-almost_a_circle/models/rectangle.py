b3512f52edcbcabf4039e6773119fd884433c3d2
khouzi <khouzem.boussaid@gmail.com>
2021-10-18 14:53:14 +0200

    class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,7 @@ class Rectangle (Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes instance of Rectangle class"""
         super().__init__(id)
         self.width = width
         self.height = height
