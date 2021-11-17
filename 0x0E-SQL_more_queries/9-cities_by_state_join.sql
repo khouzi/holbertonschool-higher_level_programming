174963a578fe1dbfefd6cd7803c311ea9ee7f9d1
root <root@LAPTOP-QLEO7F5L.localdomain>
2021-11-17 21:07:04 +0100

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-SELECT cities.id, cities.name, states.name FROM states, cities
+SELECT cities.id, cities.name, states.name FROM states, cities ORDER BY cities.id ASC;
