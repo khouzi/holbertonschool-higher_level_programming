5463386d930d791c8605a29d59fc671f3718cd4b
root <root@LAPTOP-QLEO7F5L.localdomain>
2021-11-17 20:36:10 +0100

    8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT name FROM cities WHERE states.name = "California";
+SELECT name FROM cities WHERE states.name = "California" GROUP BY cities.id DESC;
